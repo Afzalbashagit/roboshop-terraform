ff2c9eb5f9d51785cd0a457b6294beec23418d2d
Afzal Basha <afzalbasha992@gmail.com>
2023-05-11 18:08:38 +0530

    changes

--- servers.tf
+++ servers.tf
@@ -1,55 +1,4 @@
-data "aws_ami" "centos" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-}
 
-
-variable "components"{
-
-  default={
-    frontend={
-      name="frontend"
-      instance_type="t3.micro"
-    }
-    mongodb={
-      name="mongodb"
-      instance_type="t3.micro"
-    }
-    catalogue={
-      name="catalogue"
-      instance_type="t3.micro"
-    }
-    redis={
-      name="redis"
-      instance_type="t3.micro"
-    }
-    user={
-      name="user"
-      instance_type="t3.micro"
-    }
-    cart={
-      name="cart"
-      instance_type="t3.micro"
-    }
-    mysql={
-      name="mysql"
-      instance_type="t3.micro"
-    }
-    shipping={
-      name="shipping"
-      instance_type="t3.micro"
-    }
-    rabbitmq={
-      name="rabbitmq"
-      instance_type="t3.micro"
-    }
-    payment={
-      name="payment"
-      instance_type="t3.micro"
-    }
-  }
-}
 
 resource "aws_instance" "instance" {
   for_each=var.components
@@ -63,9 +12,6 @@ resource "aws_instance" "instance" {
 
 variable "instance_type"{
   default="t3.small"
-}
-data "aws_security_group" "allow-all" {
-  name="allow-all"
 }
 
 resource "aws_route53_record" "records" {
