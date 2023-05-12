709fe1b45cb6143b965174f6e29176868a073532
Afzal Basha <afzalbasha992@gmail.com>
2023-05-12 09:40:29 +0530

    changes

--- servers.tf
+++ servers.tf
@@ -22,7 +22,7 @@ resource "null_resource" "provisioner"{
       type="ssh"
       user="centos"
       password="DevOps321"
-      host=self.private_ip
+      host=aws_instance.instance[each.value["name"]].private_ip
     }
     inline=[
     "rm -rf roboshop-shell",
