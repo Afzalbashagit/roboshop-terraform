6f679ba1878cf98e09ed5b5d57eb63a04a36af61
Afzal Basha <afzalbasha992@gmail.com>
2023-05-12 09:38:49 +0530

    changes

--- servers.tf
+++ servers.tf
@@ -15,7 +15,7 @@ variable "instance_type"{
 }
 
 resource "null_resource" "provisioner"{
-  depends_on = [aws_instance,aws_route53_record]
+  depends_on = [aws_instance.instance,aws_route53_record.records]
   for_each=var.components
   provisioner "remote-exec"{
     connection{
