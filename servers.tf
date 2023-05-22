0120859ae85be1247456126b39d00d90efc441e5
afzalepam <afzalbasha992@gmail.com>
2023-05-22 22:50:19 +0530

    changes

--- servers.tf
+++ servers.tf
@@ -1,21 +1,19 @@
-module "database-servers" {
-  for_each = var.database_servers
-
-  source         = "./module"
-  component_name = each.value["name"]
-  env            = var.env
-  instance_type  = each.value["instance_type"]
-  password       = lookup(each.value, "password", "null")
-  provisioner    = true
+module "database_servers"{
+  for_each=var.database_servers
+  source="./module"
+  component_name=each.value["name"]
+  env=var.env
+  instance_type=each.value["instance_type"]
+  password=lookup(each.value,"password","null")
+  provisioner = true
 }
 
-module "app-servers" {
-  depends_on = [module.database-servers]
-  for_each   = var.app_servers
-
-  source         = "./module"
-  component_name = each.value["name"]
-  env            = var.env
-  instance_type  = each.value["instance_type"]
-  password       = lookup(each.value, "password", "null")
+module "app_servers"{
+  depends_on = [module.database_servers]
+  for_each=var.app_servers
+  source="./module"
+  component_name=each.value["name"]
+  env=var.env
+  instance_type=each.value["instance_type"]
+  password=lookup(each.value,"password","null")
 }
